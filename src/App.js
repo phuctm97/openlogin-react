ff857f64d392c3cb071f4b14d98b5a7cf75019b6
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-03-20 14:03:46 +0700

    Add Openlogin, format code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import OpenLogin from "@toruslabs/openlogin";
+import logo from "./logo.svg";
+import "./App.css";
+
+const VERIFIER = {
+  name: "Google",
+  typeOfLogin: "google",
+  clientId:
+    "221898609709-obfn3p63741l5333093430j3qeiinaa8.apps.googleusercontent.com",
+  verifier: "google-lrc",
+};
 
 function App() {
+  const [openlogin, setOpenLogin] = useState();
+
+  const onMount = async () => {
+    const obj = new OpenLogin({
+      clientId: VERIFIER.clientId,
+      iframeUrl: "http://beta.openlogin.com",
+    });
+
+    setOpenLogin(obj);
+    await obj.init();
+  };
+
+  const onLogin = async () => {
+    if (!openlogin) return;
+    if (openlogin.privKey) return;
+
+    await openlogin.login({
+      loginProvider: VERIFIER.typeOfLogin,
+      redirectUrl: "http://localhost:3000/redirect",
+    });
+  };
+
+  useEffect(() => {
+    onMount();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>You didn't login yet.</p>
+        <button className="App-button" onClick={onLogin}>
+          Login
+        </button>
       </header>
     </div>
   );
